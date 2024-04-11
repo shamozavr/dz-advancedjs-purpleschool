33a5dfe375eb83dc5418e9c78e81a0e482e60183
Roman Shashin <propeller1992@gmail.com>
2024-04-11 10:32:46 +0300

    fix: ageToCheck is an argument now, BirthDate refactored

--- 3-birthday.js
+++ 3-birthday.js
@@ -1,14 +1,13 @@
-const AGETOCHECK = 14;
-
-function idOldEnough(birthday) {
-  const birthdayArray = birthday.split('-');
+function idOldEnough(birthday, ageToCheck = 14) {
+  const [day, month, year] = birthday.split('-');
   const now = Date.now()
-  const birthdayDate = new Date(birthdayArray[2], birthdayArray[1]-1, birthdayArray[0]);
+  const birthdayDate = new Date(year, month-1, day);
   const birthdaySecs = birthdayDate.getTime();
   const ageSeconds = now - birthdaySecs;
   const age = new Date(ageSeconds).getFullYear() - 1970;
-  return age > AGETOCHECK;
+  return age > ageToCheck;
 }
 
+console.log(idOldEnough('18-12-1992', 40))
 console.log(idOldEnough('18-12-1992'))
 console.log(idOldEnough('18-06-2010'))
